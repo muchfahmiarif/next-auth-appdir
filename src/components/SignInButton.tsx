f9931aace6a4bf37f122911d1223a66c1bd1b2c9
muchfahmiarif <fahmiarif96@gmail.com>
2023-07-15 11:30:58 +0700

    add button sign in and sign out

---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,27 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+
+const SignInButton = () => {
+  const { data: session } = useSession();
+
+  if (session && session.user) {
+    return (
+      <div className="flex gap-4 ml-auto">
+        <p className="text-sky-500">{session.user.name}</p>
+        <button className="text-red-500" onClick={() => signOut()}>
+          Sign Out
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex gap-4 ml-auto">
+        <button className="text-sky-500" onClick={() => signIn()}>
+          Sign In
+        </button>
+      </div>
+    );
+  }
+};
+
+export default SignInButton;
